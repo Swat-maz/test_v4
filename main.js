ff1c8b620bf2dfc285b1f8fcd9d7523336f61068
Swat-maz <igor.mazur.selets@gmail.com>
2021-03-31 18:53:03 +0300

    chornova verstks done

--- main.js
+++ main.js
@@ -1,34 +1,60 @@
-$(function() {
+$(document).ready(function(){
+  
   $('.course__slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          dots: false,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.reviews__slider').slick({
+    // fade: true,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    fade: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 500,
+    arrows: false,
     dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+    // infinite: true,
+    // speed: 300,
+    // adaptiveHeight: true
+  });
+  
+  $('.mentors__slider').slick({
+    // fade: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 500,
+    arrows: false,
+    dots: true,
   })
 })
